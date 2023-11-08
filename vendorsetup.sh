a36c5affc23844d5a531aeb9c81dac178050607e
Jorge <jorgeim97@gmail.com>
2023-11-08 17:21:58 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_aristotle-user
-add_lunch_combo omni_aristotle-userdebug
-add_lunch_combo omni_aristotle-eng
+add_lunch_combo twrp_aristotle-user
+add_lunch_combo twrp_aristotle-userdebug
+add_lunch_combo twrp_aristotle-eng
